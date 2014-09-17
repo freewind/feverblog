fd3830206ee18f34843af9c3ca3960a8d7498e0b
Freewind <pli@thoughtworks.com>
2014-09-17 18:20:45 +0800

    ...

--- src/main/scala/io/github/freewind/Feverblog.scala
+++ src/main/scala/io/github/freewind/Feverblog.scala
@@ -4,6 +4,7 @@ final class Feverblog extends xsbti.AppMain {
   def run(configuration: xsbti.AppConfiguration): xsbti.MainResult = {
     val baseDir = configuration.baseDirectory()
     println("Hello feverblog!")
+    println("### project files:")
     baseDir.listFiles().foreach(println)
     new sbt.Exit(0)
   }
